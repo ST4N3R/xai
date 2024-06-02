9cfc7be4efe1ba2110e4eeff582f10eb84d1ec98
STANEEER <sgozdzikowski@st.swps.edu.pl>
2024-06-02 15:24:54 +0200

    Corrected values that are shown on figures

--- utils/datahandler.py
+++ utils/datahandler.py
@@ -6,53 +6,53 @@ from sklearn.model_selection import train_test_split
 class DataHandler():
     def __init__(self) -> None:
         self.df = None
+        self.scaler = StandardScaler()
 
 
     def load_data(self, path=r"C:\Programowanie\GitHub\xai\healthcare-dataset-stroke-data.csv") -> None:
+        #Wczytanie zbioru danych
         self.df = pd.read_csv(path)
 
     
     def fill_values(self) -> None:
+        #Uzupełnienie wartości pustych, w tym przypadku jest to średnia
         self.df.fillna(value=self.df['bmi'].mean(), inplace=True)
 
 
     def standarization(self) -> None:
-        scaler = StandardScaler()
-        self.df = pd.concat([pd.DataFrame(scaler.fit_transform(self.df.iloc[:, :-1]), columns=self.df.columns[:-1]), 
+        #Standaryzacja danych zgodnie ze StandardScaler
+        self.df = pd.concat([pd.DataFrame(self.scaler.fit_transform(self.df.iloc[:, :-1]), columns=self.df.columns[:-1]), 
                              self.df.iloc[:, -1]], 
                              axis=1)
         self.df.reset_index(inplace=True, drop=True)
 
 
-    def preprocess_data(self, ohe = False) -> None:
+    def preprocess_data(self, standarization=False) -> None:
+        #Przygotowanie danych do uczenia, get_dummies działa zamiast OneHotEncoder
+        #Jest możliwość dodania standaryzacji, ale on psuje wizualizacje. Nie można wtedy sprawdzić jaki wiek był w danym rekordzie
         self.df.drop(["id"], axis=1, inplace=True)
-        if ohe:
-            cat_columns = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
-            ohe = pd.get_dummies(self.df[cat_columns], prefix=cat_columns)
 
-            self.df = pd.concat([self.df, ohe], axis=1)
-            self.df.drop(cat_columns, axis=1, inplace=True)
+        cat_columns = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
+        ohe = pd.get_dummies(self.df[cat_columns], prefix=cat_columns, dtype=int)
 
+        self.df = pd.concat([self.df, ohe], axis=1)
+        self.df.drop(cat_columns, axis=1, inplace=True)
+
+        if standarization:
             self.standarization()
-        else:
-            self.df['gender'] = self.df['gender'].replace({"Male": 0, "Female": 1, "Other": -1})
-            self.df['ever_married'] = self.df['ever_married'].replace({"Yes": 1, "No": 0})
-            self.df['work_type'] = self.df['work_type'].replace({'Private': 0, 
-                                                                 'Self-employed': 1, 
-                                                                 'Govt_job': 2, 
-                                                                 'children': -1, 
-                                                                 'Never_worked': -2})
-            self.df['Residence_type'] = self.df['Residence_type'].replace({"Urban": 0, "Rural": 1})
-            self.df['smoking_status'] = self.df['smoking_status'].replace({'formerly smoked': 0, 
-                                                                           'never smoked': 1, 
-                                                                           'smokes': -1, 
-                                                                           'Unknown': 2})
 
             
-    def prepare_data(self, ohe = False) -> None:
-        self.preprocess_data(ohe)
+    def prepare_data(self, standarization=False) -> None:
+        #Główna funkcja do przygotowania danych
+        self.preprocess_data(standarization)
         self.fill_values()
     
+
+    def reverse_standarization(self, data: pd.DataFrame) -> pd.DataFrame:
+        df_reverse =pd.concat([pd.DataFrame(self.scaler.inverse_transform(data), columns=data.columns)],
+                             axis=1)
+        return df_reverse
+
 
     def get_data_split(self, test_size=0.2, seed=2021) -> list:
         X = self.df.iloc[:, :-1]
